869f19d38f562bf1e74da5697b69b1fe49787200
BGoon987 <BGoon0128@protonmail.ch>
2021-03-13 22:34:13 +0900

    Ongoing

--- MyCalendar/DateModel/DateModel.swift
+++ MyCalendar/DateModel/DateModel.swift
@@ -6,6 +6,22 @@
 //
 
 import Foundation
+import SwiftUI
+
+public class DateHelper {
+    public class func date(year: Int, month: Int, day: Int, hour: Int = 0, minute: Int = 0, seconds: Int = 0) -> Date? {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        return formatter.date(from: "\(year)-\(month)-\(day) \(hour):\(minute):\(seconds)")
+    }
+    
+    public class func dateAfter(month: Int, from baseDate: Date) -> Date? {
+        let monthToAdd = month
+        var dateComponents = DateComponents()
+        dateComponents.month = monthToAdd
+        return Calendar.current.date(byAdding: .month, value: monthToAdd ,to: baseDate)
+    }
+}
 
 extension Date {
     public var year: Int {
--- MyCalendar/Month/MonthView.swift
+++ MyCalendar/Month/MonthView.swift
@@ -10,34 +10,51 @@ import SwiftUI
 struct MonthView: View {
     
     var monthWidth = UIScreen.main.bounds.width * 0.9
-    
+    @State var presentMonth = Date().month
+    @State var presentYear = Date().year
     
     var body: some View {
         
         VStack {
             HStack {
                 Button(action: {
-                    Date().year
+//                    presentMonth = DateHelper.dateAfter(month: -1, from: Date())!.month
+                    
+                    if presentMonth > 1 {
+                        presentMonth = self.presentMonth - 1
+                    } else {
+                        presentYear = self.presentYear - 1
+                        presentMonth = 12
+                    }
                     
                     
                     
                 }, label: {
                     Image(systemName: "arrowtriangle.left.fill")
+                        .font(.largeTitle)
                 })
                 Spacer()
                 HStack {
-                    Text(Date().year.description)
-                    Text(Date().month.description)
+                    Text(presentYear.description)
+                        .font(.largeTitle)
+                    Text("/")
+                        .font(.largeTitle)
+                    Text(presentMonth.description)
+                        .font(.largeTitle)
                 }
-                
-                
                 Spacer()
                 
                 Button(action: {
-                    // Next Month move action
+                    if presentMonth <= 11 {
+                        presentMonth = self.presentMonth + 1
+                    } else {
+                        presentYear = self.presentYear + 1
+                        presentMonth = 1
+                    }
                     
                 }, label: {
                     Image(systemName: "arrowtriangle.right.fill")
+                        .font(.largeTitle)
                 })
                 
             }
